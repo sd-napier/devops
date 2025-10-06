1a351f392d1f72388f1650a99c58770d3330deb9
Socrates Davidopoulos <40785797@live.napier.ac.uk>
2025-10-06 09:12:28 +0100

    Created first JAR file with docker-compose.yml to create MySQL DB
    Signed-off-by: Socrates Davidopoulos <40785797@live.napier.ac.uk>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -26,12 +26,12 @@ public class App
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(1000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
                 System.out.println("Successfully connected");
                 // Wait a bit
-                Thread.sleep(10000);
+                Thread.sleep(1000);
                 // Exit for loop
                 break;
             }
